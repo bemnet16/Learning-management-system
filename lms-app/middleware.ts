512892508aca06aba73a3be564dace8ed1ef1b84
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-12-23 23:07:17 +0300

    Update middleware.ts

--- lms-app/middleware.ts
+++ lms-app/middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes : ["/test","/","/api/uploadthing", "/api/webhook"],
+    publicRoutes : ["/api/uploadthing", "/api/webhook"],
     // ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/uploadthing"]
 });
  
