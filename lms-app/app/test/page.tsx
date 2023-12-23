b055e18d0173c8fbde388aa78d7048cbb8f90317
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-12-23 23:06:50 +0300

    Delete lms-app/app/test/page.tsx

--- lms-app/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-const TestPage = () => {
-    return ( 
-        <div>
-            This page is available for all users weather they are logged in or not!
-        </div>
-     );
-}
- 
-export default TestPage;
